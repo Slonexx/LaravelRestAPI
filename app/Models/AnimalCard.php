dfdcc61c5ec995a6102203b2408b4b54d1716d22
Slonexx <21moriarty21@mail.ru>
2022-03-28 15:47:35 +0600

    Model Animal card

--- app/Models/AnimalCard.php
+++ app/Models/AnimalCard.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class AnimalCard extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'Nickname_Animal',
+        'Type_Animal',
+        'Age_Animal',
+        'User_id',
+    ];
 }
