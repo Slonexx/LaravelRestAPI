1af81992f911189c0fe6c265beb99cf685fc2f16
Slonexx <21moriarty21@mail.ru>
2022-03-29 13:18:15 +0600

    Animal Resource array

--- app/Http/Resources/AnimalResouce.php
+++ app/Http/Resources/AnimalResouce.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class AnimalResouce extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'Nickname_Animal' => $this->Nickname_Animal,
+            'Type_Animal' => $this->Type_Animal,
+            'Age_Animal' => $this->Age_Animal,
+            'User_id' => $this->User_id,
+        ];
     }
 }
