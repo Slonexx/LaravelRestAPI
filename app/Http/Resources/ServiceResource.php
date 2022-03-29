2844db4c204613b4ea3285e13e7c3a8bdd24a639
Slonexx <21moriarty21@mail.ru>
2022-03-30 01:30:16 +0600

    Add service Resource array

---
+++ app/Http/Resources/ServiceResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServiceResource extends JsonResource
+{
+
+    public function toArray($request)
+    {
+        return [
+            'Name_Service' => $this->Name_Service,
+            'Descriptions' => $this->Descriptions,
+        ];
+    }
+}
