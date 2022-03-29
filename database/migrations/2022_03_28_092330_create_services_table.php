b31c36a02c4205ec6919aa88b207aa856cbd0888
Slonexx <21moriarty21@mail.ru>
2022-03-30 01:29:45 +0600

    Add table service create

--- database/migrations/2022_03_28_092330_create_services_table.php
+++ database/migrations/2022_03_28_092330_create_services_table.php
@@ -6,24 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateServicesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('Name_Service');
+            $table->string('Descriptions');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('services');
