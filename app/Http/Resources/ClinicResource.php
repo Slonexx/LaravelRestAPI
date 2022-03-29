09201cb27c4c744ba89709a2a8a1f5297a65271a
Slonexx <21moriarty21@mail.ru>
2022-03-29 14:36:34 +0600

    Clinic Resource add array

---
+++ app/Http/Resources/ClinicResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClinicResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'Name_Clinic' => $this->Name_Clinic,
+            'Address' => $this->Address,
+            'URL_Picture' => $this->URL_Picture,
+        ];
+    }
+}
