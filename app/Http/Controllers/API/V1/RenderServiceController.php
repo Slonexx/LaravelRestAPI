abde4656b2f990e89f49324b79f00d0f364f6877
Slonexx <21moriarty21@mail.ru>
2022-03-30 17:26:56 +0600

    Add api Render Service controller: index, store, destroy

---
+++ app/Http/Controllers/API/V1/RenderServiceController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RenderServiceResource;
+use App\Models\RenderServices;
+use Illuminate\Http\Request;
+
+class RenderServiceController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json([
+            'Render' => RenderServiceResource::collection(RenderServices::all())
+        ],200);
+    }
+
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'Diagnosis' => 'required|string',
+            'Appointment' => 'required|string',
+            'Add_Info' => 'required|string',
+            'Animal_Cards_id' => 'required|string',
+            'Service_id' => 'required|string',
+            'Doctor_id' => 'required|string',
+            'Time_Of_Receipts_id' => 'required|string',
+        ]);
+
+        $store = RenderServices::create([
+            'Diagnosis' => $fields['Diagnosis'],
+            'Appointment' => $fields['Appointment'],
+            'Add_Info' => $fields['Add_Info'],
+            'Animal_Cards_id' => $fields['Animal_Cards_id'],
+            'Service_id' => $fields['Service_id'],
+            'Doctor_id' => $fields['Doctor_id'],
+            'Time_Of_Receipts_id' => $fields['Time_Of_Receipts_id'],
+        ]);
+        return response()->json(
+            [
+                'message' => 'add Render Service!',
+                'Render' => $store
+            ],201);
+    }
+
+    public function show($id)
+    {
+        return new RenderServiceResource(RenderServices::findOrFail($id));
+    }
+
+
+    public function destroy($id)
+    {
+        $update = RenderServices::findOrFail($id);
+        $update->delete();
+        return [
+            'message' => 'Delete Render service'
+        ];
+    }
+}
