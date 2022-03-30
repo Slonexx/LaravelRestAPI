01a1b851e5a2e61ddf59e6a977cf3b5d3e9e810f
Slonexx <21moriarty21@mail.ru>
2022-03-30 17:27:39 +0600

    add model Render service protected fillable

--- app/Models/RenderServices.php
+++ app/Models/RenderServices.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class RenderServices extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'Diagnosis',
+        'Appointment',
+        'Add_Info',
+        'Animal_Cards_id',
+        'Service_id',
+        'Doctor_id',
+        'Time_Of_Receipts_id',
+    ];
 }
