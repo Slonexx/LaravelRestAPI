575247dba7b8e1629f73966b33c42f188776bcfc
Slonexx <21moriarty21@mail.ru>
2022-04-04 14:52:21 +0600

    Factory User new

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,15 +7,13 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+
     public function definition()
     {
         return [
             'name' => $this->faker->name(),
+            'URL_Picture' => 'https://fgmo.info/wp-content/uploads/2019/03/51_2DP.jpg',
+            'phone' => $this->faker->phoneNumber(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -23,11 +21,7 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
+
     public function unverified()
     {
         return $this->state(function (array $attributes) {
