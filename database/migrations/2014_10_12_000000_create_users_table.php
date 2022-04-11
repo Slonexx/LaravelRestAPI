be05e804d91dcb3fef4edc141cc1796b98d11235
Slonexx <21moriarty21@mail.ru>
2022-04-11 16:49:57 +0600

    edit photo_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('URL_Picture');
+            $table->string('photo_id')->nullable()->default(null);
             $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
